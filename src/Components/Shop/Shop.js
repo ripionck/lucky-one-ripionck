33d5603b2e70925517320201d6b7ceffe7264f9f
ripionck <ripionchakma.421@gmail.com>
2022-03-27 22:08:15 +0600

    fixed position

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -36,23 +36,25 @@ const Shop = () => {
                 }
             </div>
             <div className="select-container">
-                <h2>Selected Items</h2>
-                {
-                    cart.map((data) => <h2 key={data.id}>{data.name}</h2>)
-                }
-                <button onClick={chooseOneRandom} className="cart-btn1">
-                    <p>CHOOSE ONE</p>
-                </button>
-                <button onClick={chooseAgain} className="cart-btn2">
-                    <p>CHOOSE AGAIN</p>
-                </button>
+                <div className="select-items">
+                    <h2>Selected Items</h2>
+                    {
+                        cart.map((data) => <h2 key={data.id}>{data.name}</h2>)
+                    }
+                    <button onClick={chooseOneRandom} className="cart-btn1">
+                        <p>CHOOSE ONE</p>
+                    </button>
+                    <button onClick={chooseAgain} className="cart-btn2">
+                        <p>CHOOSE AGAIN</p>
+                    </button>
+                </div>
             </div>
             <div className='questions'>
-                <div>
+                <div className='question-answer'>
                     <h2>How react works?</h2>
                     <p><strong>React works in declarative code.</strong> To show what we mean by declarative code, we want you to imagine the following code expressed as an app. What you picture could look like the screen below, with a navbar, a header, a filter, and a list. That's because each line of code declares what each element of the app is.</p>
                 </div>
-                <div>
+                <div className='question-answer'>
                     <h2>How useState works?</h2>
                     <p>useState is a Hook that allows you to have state variables in functional components. <strong>You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.</strong></p>
                 </div>
