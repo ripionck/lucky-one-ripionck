87eec19b383283264396d1f5812250a9a5deb70f
ripionck <ripionchakma.421@gmail.com>
2022-03-27 17:18:32 +0600

    name added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
+    const { name, picture } = props.data;
     return (
         <div className="selected-cart">
             <h3>Selected Cars</h3>
-            <p>selected:{props.length}</p>
-            <button className="cart-btn1">CHOOSE ONE</button>
-            <button className="cart-btn2">CHOOSE AGAIN</button>
+            <p>Name:{name}</p>
+            <button className="cart-btn1">
+                <p>CHOOSE ONE</p>
+            </button>
+            <button className="cart-btn2">
+                <p>CHOOSE AGAIN</p>
+            </button>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -25,9 +25,26 @@ const Shop = () => {
                 }
             </div>
             <div className="select-container">
-                <h4>Selected Items</h4>
-                {/* <p>selected:{cart.length}</p> */}
-                <Cart cart={cart}></Cart>
+                <h2>Selected Items</h2>
+                {
+                    cart.slice(0, 4).map((data) => <h2 key={data.id}>{data.name}</h2>)
+                }
+                <button className="cart-btn1">
+                    <p>CHOOSE ONE</p>
+                </button>
+                <button className="cart-btn2">
+                    <p>CHOOSE AGAIN</p>
+                </button>
+            </div>
+            <div className='questions'>
+                <div>
+                    <h2>How react works?</h2>
+                    <p><strong>React works in declarative code.</strong> To show what we mean by declarative code, we want you to imagine the following code expressed as an app. What you picture could look like the screen below, with a navbar, a header, a filter, and a list. That's because each line of code declares what each element of the app is.</p>
+                </div>
+                <div>
+                    <h2>How useState works?</h2>
+                    <p>useState is a Hook that allows you to have state variables in functional components. <strong>You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.</strong></p>
+                </div>
             </div>
         </div>
     );
